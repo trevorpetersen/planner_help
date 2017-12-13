f59aaee85a8fe36a76997c452f4de405b5df28bb
Trevor Petersen <trevorpeterse22@gmail.com>
2017-12-12 20:56:48 -0800

    Added cred.py

---
+++ python/cred.py
@@ -0,0 +1,8 @@
+#You can authenticate with either a username/password or cookie
+
+# 1st way to authenticate
+username = '' # School ID starting with a
+password = '' # password for shcool account
+
+# 2nd way to authenticate
+cookie = '' # can be generated with getCookie.py or gotten from a web browser
